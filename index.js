39ea02df966b48395e34f7f4b44acf28885e9ed9
Haarni Atte J <atte.h.1998@gmail.com>
2018-02-24 12:03:34 +0200

    Refaktoroitu useampaan tehtävän 2 mukaan

--- index.js
+++ index.js
@@ -5,41 +5,26 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const creds = require('./creds')
+const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+const mongoUrl = `mongodb://${creds.user}:${creds.pass}@ds223578.mlab.com:23578/atte-fullstack`
+mongoose
+  .connect(mongoUrl)
+  .then (() => {
+    console.log('Connected to database', mongoUrl)
+  })
+  .catch( err => {
+    console.log(err)
+  })
 
-
-module.exports = Blog
+mongoose.Promise = global.Promise
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(express.static('build'))
 
-const mongoUrl = `mongodb://${creds.user}:${creds.pass}@ds223578.mlab.com:23578/atte-fullstack`
-mongoose.connect(mongoUrl)
-mongoose.Promise = global.Promise
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
